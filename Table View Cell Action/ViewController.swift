84e213083ad39be191b308e8575e4081304817db
Mohsen Abdollahi <mohsenabdollahi@Mohsens-Mac.local>
2019-03-16 10:04:51 +0330

    Initial Commit

---
+++ Table View Cell Action/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Table View Cell Action
+//
+//  Created by Mohsen Abdollahi on 3/16/19.
+//  Copyright © 2019 Mohsen Abdollahi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
